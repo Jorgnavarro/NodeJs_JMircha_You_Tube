284dabb4b721a2de7f7f058c5ebfa31c913bc3fc
Jorgnavarro <jorgluisnavarro@gmail.com>
2023-11-18 11:45:41 -0500

    learning-about-gitHub

---
+++ Three_lesson/CommonJS/app.js
@@ -0,0 +1,4 @@
+const {add, res, div, mul, mod} = require("./calculadora.js")
+
+console.log(`add: ${add(5, 10)}, res: ${res(30, 10)}, mul: ${mul(5, 10)}, div: ${div(100, 50)}, mod: ${mod(30, 6)}`
+);
---
+++ Three_lesson/CommonJS/calculadora.js
@@ -0,0 +1,15 @@
+const add = (a,b) => a + b
+const res = (a,b) => a - b
+const mul = (a,b) => a * b
+const div = (a,b) => a / b
+const mod = (a,b) => a % b
+
+const calculator = {
+    add, 
+    res, 
+    mul, 
+    div, 
+    mod,
+}
+
+module.exports = calculator;
