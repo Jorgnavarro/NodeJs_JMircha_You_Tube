b89c1cdd1956377ff73256b71e318d02d9d8d402
Jorgnavarro <jorgluisnavarro@gmail.com>
2023-11-18 11:44:25 -0500

    async-sync-and-CommonJs-ESModules

---
+++ Three_lesson/ESModules/calculator.mjs
@@ -0,0 +1,14 @@
+const add = (a,b) => a + b
+const res = (a,b) => a - b
+const mul = (a,b) => a * b
+const div = (a,b) => a / b
+const mod = (a,b) => a % b
+
+export const calculator = {
+    add, 
+    res, 
+    mul, 
+    div, 
+    mod,
+}
+
